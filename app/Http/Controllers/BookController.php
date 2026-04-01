45f6031b26f0261a6dbd2c124aa1360f190964cd
nengwi <nengmilaningrum@gmail.com>
2026-04-01 12:58:02 +0700

    Backend: Commit BookController dan Model Book untuk fitur CRUD

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,7 +11,7 @@ class BookController extends Controller
     public function index()
     {
         return Inertia::render('Admin/Books/Index', [
-            'books' => Book::all()
+            'books' => \App\Models\Book::all()
         ]);
     }
 
