8bf3da4173a2ba08d36796f03b5f88b9895465d0
nengwi <nengmilaningrum@gmail.com>
2026-04-01 12:57:40 +0700

    Fix: Menambahkan fillable pada Model Book agar bisa menyimpan data

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * Kolom yang boleh diisi secara massal (Mass Assignment).
+     */
+    protected $fillable = [
+        'judul',
+        'penulis',
+        'penerbit',
+        'tahun_terbit',
+        'stok',
+    ];
 }
